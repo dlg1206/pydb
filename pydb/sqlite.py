9cdcab348d678123797e39a2fda20935b3897c3a
Derek Garcia <dcia811@gmail.com>
2025-04-07 14:42:26 -1000

    refactor: make CRUD methods public

--- pydb/sqlite.py
+++ pydb/sqlite.py
@@ -1,17 +1,17 @@
-"""
-File: sqlite_database.py
-Description: Sqlite database interface for handling CRUD methods data
-
-@author Derek Garcia
-"""
 import logging
 import os
 from contextlib import contextmanager
+from enum import Enum
 from pathlib import Path
 from sqlite3 import Connection, Cursor, connect, OperationalError, IntegrityError
 from typing import List, Tuple
 
-from common.table import Table
+"""
+File: sqlite.py
+Description: Sqlite database interface for handling CRUD methods data
+
+@author Derek Garcia
+"""
 
 
 class SQLiteDatabase:
@@ -62,8 +62,8 @@ class SQLiteDatabase:
                     continue
                 sql_files.append(os.path.join(root, file))
 
-        with self._open_connection() as conn:
-            with self._get_cursor(conn) as cur:
+        with self.connection() as conn:
+            with self.cursor(conn) as cur:
                 for sql_file in sql_files:
                     # Execute sql file
                     with open(sql_file, 'r') as file:
@@ -73,7 +73,7 @@ class SQLiteDatabase:
         logging.debug(f"Created database at {self._db_location}")
 
     @contextmanager
-    def _open_connection(self) -> Connection:
+    def connection(self) -> Connection:
         """
         Open a connection that can be closed on exit
 
@@ -91,7 +91,7 @@ class SQLiteDatabase:
                 conn.close()
 
     @contextmanager
-    def _get_cursor(self, connection: Connection) -> Cursor:
+    def cursor(self, connection: Connection) -> Cursor:
         """
         Open a cursor that can be closed on exit
 
@@ -109,7 +109,7 @@ class SQLiteDatabase:
             if cur:
                 cur.close()
 
-    def _insert(self, table: Table, inserts: List[Tuple[str, str | int]], on_success_msg: str = None) -> None:
+    def insert(self, table: Table, inserts: List[Tuple[str, str | int]], on_success_msg: str = None) -> None:
         """
         Generic insert into the database
 
@@ -117,8 +117,8 @@ class SQLiteDatabase:
         :param inserts: Values to insert (column, value)
         :param on_success_msg: Optional debug message to print on success (default: nothing)
         """
-        with self._open_connection() as conn:
-            with self._get_cursor(conn) as cur:
+        with self.connection() as conn:
+            with self.cursor(conn) as cur:
                 columns, values = zip(*[(e[0], e[1]) for e in inserts])  # unpack inserts
                 columns = list(columns)
                 values = list(values)
@@ -143,8 +143,8 @@ class SQLiteDatabase:
                 if on_success_msg:
                     logging.debug(on_success_msg)
 
-    def _select(self, table: Table, columns: List[str] = None,
-                where_equals: List[Tuple[str, str | int]] = None) \
+    def select(self, table: Table, columns: List[str] = None,
+               where_equals: List[Tuple[str, str | int]] = None) \
             -> List[Tuple[str | int]]:
         """
         Generic select from the database
@@ -153,10 +153,10 @@ class SQLiteDatabase:
         :param columns: optional column names to insert into (default: *)
         :param where_equals: optional where equals clause (column, value)
         """
-        with self._open_connection() as conn:
-            with self._get_cursor(conn) as cur:
+        with self.connection() as conn:
+            with self.cursor(conn) as cur:
                 # build SQL
-                columns_names = f"({', '.join(columns)})" if columns else '*'  # ( c1, ..., cN )
+                columns_names = f"{', '.join(columns)}" if columns else '*'  # ( c1, ..., cN )
                 sql = f"SELECT {columns_names} FROM {table.value}"
                 # add where clauses if given
                 if where_equals:
@@ -167,8 +167,8 @@ class SQLiteDatabase:
                 cur.execute(f"{sql};", [] if not where_equals else [clause[1] for clause in where_equals])
                 return cur.fetchall()
 
-    def _update(self, table: Table, updates: List[Tuple[str, str | int]],
-                where_equals: List[Tuple[str, str | int]] = None, on_success: str = None, amend: bool = False) -> bool:
+    def update(self, table: Table, updates: List[Tuple[str, str | int]],
+               where_equals: List[Tuple[str, str | int]] = None, on_success: str = None, amend: bool = False) -> bool:
         """
         Generic update from the database
 
@@ -179,8 +179,8 @@ class SQLiteDatabase:
         :param amend: Amend to row instead of replacing (default: False)
         :return: True if update, false otherwise
         """
-        with self._open_connection() as conn:
-            with self._get_cursor(conn) as cur:
+        with self.connection() as conn:
+            with self.cursor(conn) as cur:
                 # build SQL
                 if amend:
                     set_clause = ', '.join(f"{col} = {col} || (?)" for col, _ in updates)
@@ -209,8 +209,8 @@ class SQLiteDatabase:
                     logging.debug(on_success)
                 return cur.rowcount > 0  # rows changed
 
-    def _upsert(self, table: Table, primary_key: Tuple[str, str], updates: List[Tuple[str, str]],
-                print_on_success: bool = True) -> None:
+    def upsert(self, table: Table, primary_key: Tuple[str, str], updates: List[Tuple[str, str]],
+               print_on_success: bool = True) -> None:
         """
         Generic upsert to the database
 
@@ -220,11 +220,11 @@ class SQLiteDatabase:
         :param print_on_success: Print debug message on success (default: True)
         """
         # attempt to update
-        if not self._update(table, updates,
-                            where_equals=[primary_key],
-                            on_success=f"Updated {primary_key[0]} '{primary_key[1]}'" if print_on_success else None,
-                            amend=True):
+        if not self.update(table, updates,
+                           where_equals=[primary_key],
+                           on_success=f"Updated {primary_key[0]} '{primary_key[1]}'" if print_on_success else None,
+                           amend=True):
             # if fail, insert
             updates.append(primary_key)
-            self._insert(table, updates,
-                         on_success_msg=f"Inserted {primary_key[0]} '{primary_key[1]}'" if print_on_success else None)
+            self.insert(table, updates,
+                        on_success_msg=f"Inserted {primary_key[0]} '{primary_key[1]}'" if print_on_success else None)
--- readme.md
+++ readme.md
@@ -1,7 +1,63 @@
 # pydb
-> One database interface to rule them all. Wrapper for various python database libraries to simplify use
+
+> One database interface to rule them all. This is a wrapper for various python database libraries to simplify use
+> between them
 
 **🏗️ This is very much a WIP, things are subject to change**
 
-See each respective branch for the database interface usage. As the interfaces mature, they will gradually get merged 
+See each respective branch for the database interface usage. As the interfaces mature, they will gradually get merged
 into main
+
+## Quickstart
+
+```bash
+pip install git+https://github.com/dlg1206/pydb.git@sqlite
+```
+
+or add to `requirements.txt`
+```
+pydb @ git+https://github.com/dlg1206/pydb.git@sqlite
+```
+
+## Databases
+
+### SQLite
+
+Make sure to have a directory with all DDL `.sql` files need to create the database if making a new instance
+
+```python
+from pydb.common.entity import Table
+from pydb.sqlite import SQLiteDatabase
+
+
+# Create table enums matching your ddl
+class MyTable(Table):
+    TABLE_A = "table_a_name"
+    TABLE_B = "table_b_name"
+    ...
+    TABLE_N = "table_n_name"
+
+
+# Use generic interface
+my_database = SQLiteDatabase("path/to/ddl")
+apricons = my_database.select(MyTable.TABLE_N)  # SELECT * FROM table_n_name
+
+
+# Create your custom interface here
+class MyDatabase(SQLiteDatabase):
+
+    # call super to create instance
+    def __init__(self, db_location: str, ddl_location: str = None, force_rebuild: bool = False):
+        super().__init__(db_location, ddl_location, force_rebuild)
+
+    # example method using prebuilt CRUD
+    def count_apricorns(self, color: str) -> int:
+        return len(self.select(MyTable.TABLE_N, where_equals=[('color', color)]))
+
+    # example method using custom sql
+    def remove_all_apricons(self, color: str) -> None:
+        with self.open_connection() as conn:
+            with self.get_cursor(conn) as cur:
+                cur.execute("DELETE FROM table_n_name WHERE color = ?;", (color,))
+                conn.commit()
+```
