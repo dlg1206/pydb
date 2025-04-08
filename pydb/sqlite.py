00a1df96ba59a43410fc521f74afabffd8f87d81
Derek Garcia <dcia811@gmail.com>
2025-04-07 14:37:09 -1000

    refactor!: replace custom logger with native python

--- pydb/sqlite.py
+++ pydb/sqlite.py
@@ -4,14 +4,13 @@ Description: Sqlite database interface for handling CRUD methods data
 
 @author Derek Garcia
 """
+import logging
 import os
 from contextlib import contextmanager
 from enum import Enum
 from pathlib import Path
 from sqlite3 import Connection, Cursor, connect, OperationalError, IntegrityError
 from typing import List, Tuple
-
-from log.logger import logger
 
 
 class Table(Enum):
@@ -25,25 +24,22 @@ class SQLiteDatabase:
     Generic interface for accessing a SQLite Database
     """
 
-    def __init__(self, db_location: str, ddl_location: str = None, force_rebuild: bool = False,
-                 handle_errors: bool = True):
+    def __init__(self, db_location: str, ddl_location: str = None, force_rebuild: bool = False):
         """
         Open interface to SQL db. If it doesn't exist, create new database
 
         :param db_location: Location of the sqlite database
         :param ddl_location: Path to DDL to construct the database
         :param force_rebuild: Force rebuild the DB (default: false)
-        :param handle_errors: Handle non-fatal errors, if false will re-throw errors (default: true)
         """
         self._db_location = db_location if db_location.endswith('.db') else f"{db_location}.db"  # ensure '.db' file ext
-        self._handle_errors = handle_errors
 
         # If db exists, don't rebuild
         if os.path.exists(self._db_location) and not force_rebuild:
-            logger.info(f"Using database at {self._db_location}")
+            logging.debug(f"Using database at {self._db_location}")
             return
         if force_rebuild:
-            logger.warn("Force rebuilding the database")
+            logging.warning("Force rebuilding the database")
 
         # building new or force rebuilding if here
 
@@ -55,11 +51,11 @@ class SQLiteDatabase:
 
         # delete old iff exists
         if os.path.exists(self._db_location):
-            logger.debug_msg(f"Deleting {self._db_location}")
+            logging.debug(f"Deleting {self._db_location}")
             os.remove(self._db_location)
 
         # build db
-        logger.info(f"Creating database at '{self._db_location}'")
+        logging.debug(f"Creating database at '{self._db_location}'")
         db_dir = os.path.dirname(self._db_location)
         Path(db_dir).mkdir(parents=True, exist_ok=True)
         sql_files = []
@@ -73,13 +69,13 @@ class SQLiteDatabase:
 
         with self._open_connection() as conn:
             with self._get_cursor(conn) as cur:
-                for sql_file in logger.get_data_queue(sql_files, 'Creating Database', 'table'):
+                for sql_file in sql_files:
                     # Execute sql file
                     with open(sql_file, 'r') as file:
-                        logger.debug_msg(f"Loading {sql_file}")
+                        logging.debug(f"Loading {sql_file}")
                         cur.executescript(file.read())
 
-        logger.info(f"Created database at {self._db_location}")
+        logging.debug(f"Created database at {self._db_location}")
 
     @contextmanager
     def _open_connection(self) -> Connection:
@@ -93,7 +89,7 @@ class SQLiteDatabase:
             conn = connect(self._db_location)
             yield conn
         except OperationalError as oe:
-            logger.fatal(oe)
+            logging.critical(oe)
         finally:
             if conn:
                 conn.rollback()  # clear any uncommitted transactions
@@ -113,7 +109,7 @@ class SQLiteDatabase:
             cur.execute('PRAGMA foreign_keys = ON;')  # enforce foreign keys
             yield cur
         except OperationalError as oe:
-            logger.fatal(oe)
+            logging.critical(oe)
         finally:
             if cur:
                 cur.close()
@@ -142,17 +138,15 @@ class SQLiteDatabase:
                     conn.commit()
                 except IntegrityError as ie:
                     # duplicate entry
-                    if not self._handle_errors:
-                        raise ie
-                    logger.debug_msg(f"{ie.sqlite_errorname} | {table.value} | ({values})")
+                    logging.debug(f"{ie.sqlite_errorname} | {table.value} | ({values})")
+                    raise ie
                 except OperationalError as oe:
                     # failed to insert
-                    if not self._handle_errors:
-                        raise oe
-                    logger.error_exp(oe)
+                    logging.error(oe)
+                    raise oe
                 # print success message if given one
                 if on_success_msg:
-                    logger.debug_msg(on_success_msg)
+                    logging.debug(on_success_msg)
 
     def _select(self, table: Table, columns: List[str] = None,
                 where_equals: List[Tuple[str, str | int]] = None) \
@@ -213,13 +207,11 @@ class SQLiteDatabase:
                     conn.commit()
                 except OperationalError as oe:
                     # failed to update
-                    if not self._handle_errors:
-                        raise oe
-                    logger.error_exp(oe)
-                    return False
+                    logging.error(oe)
+                    raise oe
                 # print success message if given one
                 if cur.rowcount > 0 and on_success:
-                    logger.debug_msg(on_success)
+                    logging.debug(on_success)
                 return cur.rowcount > 0  # rows changed
 
     def _upsert(self, table: Table, primary_key: Tuple[str, str], updates: List[Tuple[str, str]],
