aa9e6c209f47f4be12893e36d0708c022547b760
Derek Garcia <dcia811@gmail.com>
2026-02-09 17:35:53 -1000

    feat: Add generic table

---
+++ pydb/common/table.py
@@ -0,0 +1,13 @@
+"""
+File: table.py
+Description: Generic table to be expanded on by implementations
+
+@author Derek Garcia
+"""
+from enum import Enum
+
+
+class Table(Enum):
+    """
+    Generic table to be expanded on by implementations
+    """
